2af2fb724392d1e8da254ce66cc9aaea14bb01eb
Jalle Van Goidsenhoven <jalle.van.goidsenhoven@student.ehb.be>
2019-01-19 23:24:40 +0100

    added comments

--- Assignment 5/datastream/server.js
+++ Assignment 5/datastream/server.js
@@ -10,6 +10,7 @@ app.listen(8000, () => {
     setInterval(changedataToSend, 1000);
     setInterval(sendUDP, 1000);
 });
+
 //send OSC message
 var osc = require("osc");
 var udpPort = new osc.UDPPort({
@@ -24,6 +25,7 @@ udpPort.on("error", function (error) {
     console.log("An error occurred: ", error.message);
 });
 
+//send OSC message with UDP
 function sendUDP() {
     udpPort.send({
         address: "/number",
@@ -64,31 +66,29 @@ function sendUDP() {
 
 //api key: OZV5M9AT5WT8IM15
 //https://www.alphavantage.co/
-var datastream = 'TIME_SERIES_INTRADAY';
-var company = 'MSFT';
-var interval = '1min';
-
+//variables for api
 var dataToSend = 0;
 var indexCounter;
 var RealTimeData = [];
 
-var ApiKeys = ['5NXUYX3JA52LHZLR', '5NXUYX3JA52LHZLR', '802NUQMS422UQW7H', '802NUQMS422UQW7H'];
-var exchangerates = [];
-var currencyFrom = 'USD';
 var eur = 0;
 var jpy = 0;
 var btc = 0;
 var gpb = 0;
 var min = 0;
 var max = 0;
-//getExchangeRates();
-//getData();
-//setInterval(changedataToSend, 1000);
-//setInterval(getExchangeRates, 1000);
-//setInterval(function(){console.log(exchangerates)}, 1000);
 
+//var for reading json response
+const firstLevelExchange = 'Realtime Currency Exchange Rate';
+const categoryOfExchange = '5. Exchange Rate';
+
+const firstLevelStock = "Time Series (1min)";
+const categoryOfStock = "2. high";
+
+
+//get current stockvalue of selected company
 function getData() {
-    request('https://www.alphavantage.co/query?function=' + datastream + '&symbol=' + company + '&interval=' + interval + '&apikey=OZV5M9AT5WT8IM15', {
+    request('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=OZV5M9AT5WT8IM15', {
         json: true
     }, (err, res, body) => {
         if (err) {
@@ -99,60 +99,66 @@ function getData() {
     });
 }
 
+//get exchangerates for certain currencies
 function getExchangeRates() {
 
-    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + currencyFrom + '&to_currency=EUR&apikey=5NXUYX3JA52LHZLR', {
+    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=5NXUYX3JA52LHZLR', {
         json: true
     }, (err, res, body) => {
         if (err) {
             return console.log(err);
         } else {
-           eur = body['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+           eur = body[firstLevelExchange][categoryOfExchange];
         }
     });
 
-    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + currencyFrom + '&to_currency=CNY&apikey=5NXUYX3JA52LHZLR', {
+    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=CNY&apikey=5NXUYX3JA52LHZLR', {
         json: true
     }, (err, res, body) => {
         if (err) {
             return console.log(err);
         } else {
-           jpy = body['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+           jpy = body[firstLevelExchange][categoryOfExchange];
         }
     });
 
-    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + currencyFrom + '&to_currency=AUD&apikey=802NUQMS422UQW7H', {
+    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=AUD&apikey=802NUQMS422UQW7H', {
         json: true
     }, (err, res, body) => {
         if (err) {
             return console.log(err);
         } else {
-           btc = body['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+           btc = body[firstLevelExchange][categoryOfExchange];
         }
     });
 
-    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + currencyFrom + '&to_currency=GBP&apikey=802NUQMS422UQW7H', {
+    request(' https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=GBP&apikey=802NUQMS422UQW7H', {
         json: true
     }, (err, res, body) => {
         if (err) {
             return console.log(err);
         } else {
-           gpb = body['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+           gpb = body[firstLevelExchange][categoryOfExchange];
         }
     });
 }
 
+//reformat the data from the getData JSON response
+//Read data and push relevant info into array
+
 function procesData(d) {
     RealTimeData = [];
-    var minutes = d["Time Series (1min)"];
+    var minutes = d[firstLevelStock];
     for (let minute in minutes) {
-        RealTimeData.push(minutes[minute]["2. high"]);
+        RealTimeData.push(minutes[minute][categoryOfStock]);
     }
     indexCounter = RealTimeData.length - 1;
     max = Math.max(...RealTimeData);
     min = Math.min(...RealTimeData);
 }
 
+//send a new number every second
+//when the end is reached get new data
 function changedataToSend() {
     dataToSend = RealTimeData[indexCounter];
     if (indexCounter == 0) {
@@ -160,14 +166,4 @@ function changedataToSend() {
         return;
     }
     indexCounter--;
-    //console.log(dataToSend)
-    // sendUDP();
 }
-
-app.route('/api/stockdata').get((req, res) => {
-    res.send('' + dataToSend + '');
-});
-
-app.route('/api/exchangerate').get((req, res) => {
-    res.send([exchangerates]);
-});
